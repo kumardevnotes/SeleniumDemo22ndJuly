39fba40b5890646ffc3326e8cc456d6d3ab238fe
kumardevnotes <kumar.thoudishetti@gmail.com>
2023-07-29 22:23:26 +0530

    added maven project files

---
+++ src/test/java/JavaDemo.java
@@ -0,0 +1,12 @@
+
+public class JavaDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello QA World!");
+		System.out.println("Hello Dev World!");
+		System.out.println("Hello Database World!");
+	
+	}
+
+}
